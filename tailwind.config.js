522ed1dd402202a54987b44e92c3f39f5b4fb700
Samandar-14188 <157939075+Samandar-14188@users.noreply.github.com>
2024-12-30 18:42:37 +0500

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./**/*.html", "./**/*.js"], // Matches all HTML and JS files in any folder
   theme: {
     extend: {},
   },
